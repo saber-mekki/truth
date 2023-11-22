44c717a5d4233a312270e306ff97266283373be9
Saber <saber.mekki@aureskonnect.com>
2023-11-22 14:41:32 +0100

    chore:clean-up

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image'
 import backgroundImage from '../public/photo/back.jpg'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import CardComponent from './CardComponent'
+
+import  Zoom  from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 import ReactGA from 'react-ga'
 
 export default function Home() {
@@ -18,7 +21,9 @@ export default function Home() {
     womenKilled: '+3.1k',
     blessings: '+30k',
   }
-  
+
+  const [isZoomed, setIsZoomed] = useState(false)
+
   useEffect(() => {
     ReactGA.pageview(window.location.pathname + window.location.search)
   }, [])
@@ -60,8 +65,28 @@ export default function Home() {
           justifyContent: 'center',
         }}
       >
+        <Zoom
+        overlayBgColorEnd="rgba(0, 0, 0, 0.85)"
+        isOpen={isZoomed}
+        setIsOpen={setIsZoomed}
+      >
+        <div key="first" className="pb-5 pt-5">
+          <Image
+            style={{ borderRadius: '50px' }}
+            src="/gaza/now.webp"
+            alt="Photo"
+            width={1500}
+            height={1000}
+          />
+        </div>
+         </Zoom>
         {new Array(allPhotoNumber).fill(0).map((image: any, index: number) => (
           <div key={index} className="pb-5 pt-5">
+            <Zoom
+        overlayBgColorEnd="rgba(0, 0, 0, 0.85)"
+        isOpen={isZoomed}
+        setIsOpen={setIsZoomed}
+      >
             <Image
               style={{ borderRadius: '50px' }}
               src={`/gaza/gaza${index}.jpg`}
@@ -69,7 +94,9 @@ export default function Home() {
               width={1500}
               height={1000}
             />
+              </Zoom>
           </div>
+          
         ))}
       </div>
 

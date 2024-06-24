f4e8e2ccc613b8aa0b42fcf4b367eebe24f86a49
Saif <saifbenyaagoub@gmail.com>
2024-06-24 17:53:38 +0200

    feat:Internationalization

--- pages/CardComponent.tsx
+++ pages/CardComponent.tsx
@@ -1,7 +1,8 @@
 // components/CardComponent.tsx
 
 import React from 'react'
-
+import eng from 'lang/eng.json' // Import English translations
+import fr from 'lang/fr.json' // Import French translations
 interface CardProps {
   deathsToday: any
   missing: any
@@ -10,7 +11,9 @@ interface CardProps {
   Wounded: any
   studentsKilled: any
   studentsWounded: any
+  lang?: 'eng' | 'fr'
 }
+
 const CardComponent: React.FC<CardProps> = ({
   deathsToday,
   missing,
@@ -19,38 +22,40 @@ const CardComponent: React.FC<CardProps> = ({
   Wounded,
   studentsKilled,
   studentsWounded,
+  lang = 'eng',
 }) => {
+  const language = lang === 'fr' ? fr : eng
   return (
     <div className="card">
       <ul>
         <li className="list-item">
-          <a className="li_clz">Number of deaths :</a>
+          <a className="li_clz">{language.deathsToday}</a>
           <a className="number_clz">{deathsToday}</a>
         </li>
         <li className="list-item">
-          <a className="li_clz">Number of missings :</a>
+          <a className="li_clz">{language.missing}</a>
           <a className="number_clz">{missing}</a>
         </li>
         <br />
         <br />
         <li className="list-item">
-          <a className="li_clz">Number of children Killed :</a>{' '}
+          <a className="li_clz">{language.childrenKilled}</a>{' '}
           <a className="number_clz">{childrenKilled}</a>
         </li>
         <li className="list-item" id="x">
-          <a className="li_clz">Number of women Killed :</a>{' '}
+          <a className="li_clz">{language.womenKilled}</a>{' '}
           <a className="number_clz"> {womenKilled}</a>
         </li>
         <li className="list-item">
-          <a className="li_clz">Number of Wounded :</a>
+          <a className="li_clz">{language.Wounded}</a>
           <a className="number_clz"> {Wounded}</a>
         </li>
         <li className="list-item">
-          <a className="li_clz">Number of students Killed :</a>
+          <a className="li_clz">{language.studentsKilled}</a>
           <a className="number_clz">{studentsKilled}</a>
         </li>
         <li className="list-item">
-          <a className="li_clz">Number of students Wounded :</a>
+          <a className="li_clz">{language.studentsWounded}</a>
           <a className="number_clz">{studentsWounded}</a>
         </li>
       </ul>
